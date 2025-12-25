f4d099902cba00b462b72e983ac477adc4bd4189
Yassaf <yousefsekari96@gmail.com>
2025-12-25 16:11:57 +0300

    Initial Commit

---
+++ EasyJob/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyJob
+//
+//  Created by MAC on 25.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EasyJob/EasyJobApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyJobApp.swift
+//  EasyJob
+//
+//  Created by MAC on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct EasyJobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
